b7da94f052bdb85feb08bd0f3f3c313d1d584ab3
Lila Sicuan <viktoria.sicuan@gmail.com>
2023-04-08 12:23:44 +0800

    Updated CSS

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var validChars = "ABCDEFabcdef0123456789";
 var alertDisplay = document.getElementById("alert");
+var convertSuccess = false;
 
-document.getElementById("enter").addEventListener("click", () => {
+document.getElementById("convert").addEventListener("click", () => {
     var unicode = document.getElementById("unicode").value;
     reset_values();
 
     if(unicode.length == 0) {
-        alertDisplay.innerHTML = "Invalid input!";
+        alertDisplay.innerHTML = "No null input!";
+        convertSuccess = false;
         return -1;
     }
     
     for(var i = 0; i < unicode.length; i++) {
         if(!validChars.includes(unicode.charAt(i))) {
             alertDisplay.innerHTML = "Invalid input!";
-            console.log("buffoon");
+            convertSuccess = false;
             return -1;
         }
     }
 
+    convertSuccess = true;
     document.getElementById("utf-8").value = get_UTF_8(unicode);
     document.getElementById("utf-16").value = get_UTF_16(unicode);
     document.getElementById("utf-32").value = get_UTF_32(unicode);
@@ -25,6 +28,16 @@ document.getElementById("enter").addEventListener("click", () => {
 
 // TODO: Export to text file when #export is clicked
 document.getElementById("export").addEventListener("click", () => {
+    var fileName = "output.txt"
+
+    if(convertSuccess) {
+        alertDisplay.classList.add("green-alert");
+        alertDisplay.innerHTML = "Successfully exported to " + fileName;
+    }
+    else {
+        alertDisplay.classList.add("red-alert");
+        alertDisplay.innerHTML = "Cannot export to text file!";
+    }
 
 });
 
@@ -49,4 +62,6 @@ function reset_values() {
     document.getElementById("utf-32").value = "";
 
     alertDisplay.innerHTML = "";
+    alertDisplay.className = "";
+    alertDisplay.classList.add("red-alert");
 }
